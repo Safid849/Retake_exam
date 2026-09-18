c474a5131c5824f2b08defb270a0e5342baedada
Zinedis <nantenainabakari@gmail.com>
2026-09-18 10:14:26 +0300

    feat: add class Client

---
+++ src/main/java/practice/prog2/Client.java
@@ -0,0 +1,75 @@
+package practice.prog2;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@EqualsAndHashCode
+@ToString
+
+public class Client {
+    private String nom;
+    private String prenom;
+    private LocalDate dateNaissance;
+    private String adresse;
+    private String adresseMail;
+    private String telephone;
+
+    public Client(String nom, String prenom,LocalDate dateNaissance, String adresse, String adresseMail, String telephone) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        this.adresse = adresse;
+        this.adresseMail = adresseMail;
+        this.telephone = telephone;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public LocalDate getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(LocalDate dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getAdresseMail() {
+        return adresseMail;
+    }
+
+    public void setAdresseMail(String adresseMail) {
+        this.adresseMail = adresseMail;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+}
---
+++ src/main/java/practice/prog2/Vetement.java
@@ -0,0 +1,4 @@
+package practice.prog2;
+
+public class Clothes {
+}
