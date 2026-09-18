b9f5e598defe80eb6559fff690bc31ded28bfa6a
Zinedis <nantenainabakari@gmail.com>
2026-09-18 10:17:01 +0300

    feat: add abstract class Vetement

--- src/main/java/practice/prog2/Vetement.java
+++ src/main/java/practice/prog2/Vetement.java
@@ -1,4 +1,34 @@
 package practice.prog2;
 
-public class Clothes {
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
+public abstract class Vetement {
+    private String nom;
+    private TailleEnum taille;
+    private Double prixUnitaire;
+    private String materieUtilise;
+
+    public Vetement(String nom, TailleEnum taille, Double prixUnitaire, String materieUtilise) {
+        this.nom = nom;
+        this.taille = taille;
+        this.prixUnitaire = prixUnitaire;
+        this.materieUtilise = materieUtilise;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public TailleEnum getTaille() {
+        return taille;
+    }
+
+    public Double getPrixUnitaire() {
+        return prixUnitaire;
+    }
+
+    public String getMaterieUtilise() {
+        return materieUtilise;
+    }
 }
